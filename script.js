6dab609368a80e682ae1e73930fcb14b87ae14d8
Hamza Fafouri <hfafouri@e1r5p11.1337.ma>
2025-10-06 16:58:57 +0100

    push

--- script.js
+++ script.js
@@ -5,9 +5,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
     Form.addEventListener("submit", function()
     {
+            var isValid = true;
+            let messages = [] ;
+            event.preventDefault();
             const username = document.getElementById("username").value.trim();
             const email = document.getElementById("email").value.trim();
             const password = document.getElementById("password").value.trim();
+
+            if (username.length < 3)
+            {
+                isValid = false;
+                messages.push("username less than 3");
+            }
+
+            if (!email.includes("@") || !email.includes("."))
+            {
+                isValid = false;
+                messages.push("email not valid format");
+            }
+            if (password.length < 8 )
+            {
+                isValid = false;
+                messages.push("password length is not valid");
+            }
+            
+            feedbackDiv.style.display = "block"; 
+            if (isValid) 
+            {
+                feedbackDiv.textContent = "Registration successful!";
+                feedbackDiv.style.color = "#28a745"; 
+            } 
+            else 
+            {
+                feedbackDiv.innerHTML = messages.join("<br>");
+                feedbackDiv.style.color = "#dc3545";
+            }
             
             console.log("Username:", username);
             console.log("Email:", email);
